583ebc84eec05f830b0b231147157bb3dbfe7392
EtheriousShadow <hassani.salah2005@gmail.com>
2025-10-26 16:25:27 +0100

    Test FizzBuzz v2 FAIL

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,11 +3,16 @@ from src.fizzbuzz import affiche
 
 def test_affiche_sans_parametre():
     """
-    VÈrifie que la fonction affiche() renvoie bien une chaÓne contenant
-    Fizz, Buzz, et FrisBee sur les nombres de 1 ‡ 100.
+    V√©rifie que la fonction affiche() renvoie bien une cha√Æne contenant
+    Fizz, Buzz, et FrisBee sur les nombres de 1 √† 100.
     """
     result = affiche()
     assert "Fizz" in result
     assert "Buzz" in result
     assert "FrisBee" in result
     assert result.startswith("12Fizz")
+
+def test_affiche_avec_parametre():
+    """Teste la version avec un param√®tre n."""
+    result = affiche(15)
+    assert result == "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
