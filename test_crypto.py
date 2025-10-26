15bd7618a4c5c69e82e631a1693f3b151c15db3e
EtheriousShadow <hassani.salah2005@gmail.com>
2025-10-26 16:31:30 +0100

    Test crypt avec pas FAIL

--- test_crypto.py
+++ test_crypto.py
@@ -4,3 +4,10 @@ from src.crypto import crypt
 def test_crypt_simple():
     """Teste le cryptage simple (+1 ASCII)."""
     assert crypt("abc") == "bcd"
+
+def test_crypt_avec_pas():
+    """Teste le cryptage avec pas variable."""
+    result = crypt("abc", 2)
+    assert result.startswith("cde")
+    assert result.endswith("2")
+    
