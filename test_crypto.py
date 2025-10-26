2a7b5e8f2e7b89ab034fd3bf08c6d76a43097f90
Dokuseivl <klughertz.jason@gmail.com>
2025-10-26 16:35:30 +0100

    Test decrypt FAIL

--- test_crypto.py
+++ test_crypto.py
@@ -1,5 +1,6 @@
 import pytest
 from crypto import crypt
+from crypto import decrypt
 
 def test_crypt_simple():
     """Teste le cryptage simple (+1 ASCII)."""
@@ -11,3 +12,7 @@ def test_crypt_avec_pas():
     assert result.startswith("cde")
     assert result.endswith("2")
     
+def test_decrypt_message():
+    """Teste le décryptage d’un message."""
+    msg = crypt("Hello!", 3)
+    assert decrypt(msg) == "Hello!"
